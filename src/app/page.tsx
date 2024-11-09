43329fa32825c0dfbb89f88f8ab1b7a76091af36
Aarsh Shah <aarsh.shah@arcurve.com>
2024-11-09 13:43:53 -0700

    Added Main Side Bar Component
    Signed-off-by: Aarsh Shah <aarsh.shah@arcurve.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 import { MapContainer, TileLayer } from "react-leaflet";
+import MainSideBar from "@/components/MainSideBar";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <MapContainer
-          center={[51.505, -0.09]}
-          zoom={2}
-          className="h-[100vh] w-[100vh]"
+      <main>
+          <MainSideBar />
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={3}
+            className="h-[100vh] w-[100vw] z-0"
+            worldCopyJump={true}
+            maxBounds={[
+                [-85, -180], // Southwest corner
+                [85, 180]    // Northeast corner
+            ]}
+            maxBoundsViscosity={1.0} // Smoothly restricts panning at the edges
         >
           <TileLayer
             url={
@@ -18,8 +26,8 @@ export default function Home() {
             }
             accessToken="WYUholf8Kd9VFs67vN1lrIKrzlTv0KTcdtwi0KWfubYj6XCRgnXUKOaDZ6cwq9uB"
           />
+
         </MapContainer>
       </main>
-    </div>
   );
 }
