cc27efe9c8e0433b2699599d55be8438a017e388
AarshShah9 <aarshshah99@gmail.com>
2024-11-10 10:17:32 -0700

    Pause
    Signed-off-by: AarshShah9 <aarshshah99@gmail.com>

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { fetchAndParseRSS } from "@/server/news";
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
 
 type HomeProps = {
   geoJson: GeoJSON.GeoJSON | null;
 };
-
+  
 export default function Home({ geoJson }: HomeProps) {
+
+  useEffect(() => {
+    fetchAndParseRSS("Floods in Uganada").then((d) => console.log(d))
+  }, [])
+  
+
   const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
   return <main>{geoJson && <Map geoJson={geoJson} />}</main>;
